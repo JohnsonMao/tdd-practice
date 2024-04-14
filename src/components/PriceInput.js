b3a7eae89a536a364eef87834e1d8653da57b812
Johnson Mao <tutelary.maomao@gmail.com>
2024-04-14 20:22:08 +0800

    feature: price input add comma format

--- src/components/PriceInput.js
+++ src/components/PriceInput.js
@@ -1,20 +1,44 @@
+import { useState } from 'react';
 import Field from './base/Field';
 import Group from './base/Group';
 import Input from './base/Input';
 import Label from './base/Label';
 import Text from './base/Text';
+import { addComma } from '../utils';
 
-export default function PriceInput({ className }) {
+export default function PriceInput({
+  name,
+  className = '',
+  errorMessage = '',
+  value = 0,
+}) {
+  const [price, setPrice] = useState(String(value));
+
+  const handleChange = ({ target }) => {
+    const number = addComma(target.value.replace(/,/g, ''));
+    if (number) {
+      setPrice(number);
+      return;
+    }
+    setPrice(target.value);
+  };
+
   return (
-    <Field name="a" className={className}>
+    <Field name={name} className={className}>
       <Label>入住費用 (每人每晚)</Label>
       <Group>
         <Text>TWD</Text>
-        <Input $isError />
+        <Input
+          $isError={!!errorMessage}
+          value={price}
+          onChange={handleChange}
+        />
       </Group>
-      <Text className="p-2 font-bold rounded-sm" $isError>
-        不可以為空白
-      </Text>
+      {errorMessage && (
+        <Text className="p-2 font-bold rounded-sm" $isError>
+          {errorMessage}
+        </Text>
+      )}
       <Text className="mt-1 text-right">輸入 0 表示免費</Text>
     </Field>
   );
